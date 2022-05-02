3fe91f1f030f0eb39d5acbfd1c18088b24848146
18104 <18104@gmail.com>
2022-05-02 12:41:25 +1200

    java

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def render_homepage():
+    return render_template("home.html")
+
+@app.route('/people')
+def render_people():
+    return render_template("people.html")
+
+
+
+app.run(host="0.0.0.0")
---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+function titleCapitialise() {
+    document.getElementById('titleToChange').innerHTML="The Official English Dictionary";
+}
+
+function titleRevert() {
+    document.getElementById('titleToChange').innerHTML="The 0fficial English Dictionary";
+}
+
+// This easter egg was created by Michael Brown
